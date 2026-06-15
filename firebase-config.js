dc4cab44d023418d775b470502a8949e9563d36a
SpiderWick389 <marcelohenriquedejesus4@gmail.com>
2026-06-15 00:48:19 -0300

    configure firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 window.FINANCEIRO_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyDjTmoHnY6EhV88ftWGLpxuRhB2j_xrjRg",
+  authDomain: "financas1-c78e4.firebaseapp.com",
+  projectId: "financas1-c78e4",
+  storageBucket: "financas1-c78e4.firebasestorage.app",
+  messagingSenderId: "20269628459",
+  appId: "1:20269628459:web:d18eeb65fac3576f04fe6e",
 };
 
 window.FINANCEIRO_FAMILY_ID = "casal-principal";
